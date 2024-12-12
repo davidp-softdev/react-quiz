e3dcb36b31d049db73d8df1e84cd295bb755bb94
David P <david.perli2@gmail.com>
2024-12-12 16:00:27 +1000

    StartScreen and data loading

--- src/Loader.js
+++ src/Loader.js
@@ -1,7 +1,7 @@
 export default function Loader() {
   return (
     <div className="loader-container">
-      <div className="loader"></div>
+      <div className="spinner-3"></div>
       <p>Loading questions...</p>
     </div>
   );
